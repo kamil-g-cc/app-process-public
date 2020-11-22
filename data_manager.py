0a0fdb4dab0521b7d1c817e185d427c076aed80f
Kamil Grondys <kamil.grondys@codecool.com>
2020-11-22 13:48:50 +0100

    adding mentors_by_city
    adding filter mentors_by_city

--- data_manager.py
+++ data_manager.py
@@ -27,6 +27,15 @@ def get_mentors_by_last_name(cursor: RealDictCursor, last_name: str) -> list:
     return cursor.fetchall()
 
 @database_common.connection_handler
+def get_mentors_by_city(cursor: RealDictCursor, city: str) -> list:
+    query = """
+        SELECT first_name, last_name, city
+        FROM mentor
+        WHERE city = %(city)s"""
+    cursor.execute(query, {'city': city})
+    return cursor.fetchall()
+
+@database_common.connection_handler
 def get_citites(cursor: RealDictCursor) -> list:
     query = """
         SELECT DISTINCT city from mentor;
--- main.py
+++ main.py
@@ -13,9 +13,13 @@ def index():
 @app.route('/mentors')
 def mentors_list():
     mentor_name = request.args.get('mentor-last-name')
+    city = request.args.get('city-name')
+
     cities = data_manager.get_citites()
     if mentor_name:
         mentor_details = data_manager.get_mentors_by_last_name(mentor_name)
+    elif city:
+        mentor_details = data_manager.get_mentors_by_city(city)
     else:
         mentor_details = data_manager.get_mentors()
 
